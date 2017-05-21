376dd8f60d6b419eee41b0b60343044b523c8bbe
Jeremy Dothager <jeremydothager@gmail.com>
2017-05-21 01:11:20 -0500

    images added to gallery

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// credit to https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
